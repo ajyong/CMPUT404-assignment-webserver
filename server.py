171eaa79c45bc7f22453edb02ef050828940b093
ajyong <ajyong@ualberta.ca>
2014-01-16 02:41:30 -0700

    Code cleaned up, commented

--- server.py
+++ server.py
@@ -32,64 +32,85 @@ from time import gmtime, strftime
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
+    def get_datetime(self):
+        return "Date: " + strftime("%a, %d %b %Y %X GMT", gmtime()) + "\n"
 
     def handle(self):
+        self.content_length = "Content-Length: "
+        self.content_type = "Content-Type: "
+        self.location = ""
         self.message_body = ""
         self.status_line = "HTTP/1.1 "
-        self.content_type = "Content-Type: "
-        self.content_length = "Content-Length: "
 
-        # Check against this path to ensure we only serve files within the
+        # We check against this path to ensure we only serve files within the
         # www folder.
         self.www_path = os.path.join(os.getcwd(), "www")
 
         self.data = self.request.recv(1024).strip()
-
         request_lines = self.data.splitlines()
         first_line_data = request_lines[0].split()
 
         if first_line_data[0] != "GET":
-            self.status_line += "501 NOT IMPLEMENTED\n"
+            self.status_line += "501 Not Implemented\n"
             self.content_type += "text/html\n"
-            self.message_body = "<html><body>HTTP/1.1 501 NOT IMPLEMENTED" \
-                                "</body></html>\n"
+            self.message_body = "<html><body><h1>501 NOT IMPLEMENTED" \
+                                "</h1></body></html>\n"
         else:
+            requested_path = os.path.normpath(os.path.join(self.www_path +
+                                              first_line_data[1]))
+
+            # For requests ending in "/", append index.html to the end of the
+            # requested file path.
             if first_line_data[1].endswith("/"):
-                self.content_type += "text/html\n"
-                requested_file_path = os.path.normpath(
-                                        os.path.join(self.www_path +
-                                                     first_line_data[1],
-                                                     "index.html"))
-            else:
-                self.content_type += str(mimetypes.guess_type(
-                                            first_line_data[1],
-                                            strict=True)[0]) + "\n"
-                requested_file_path = os.path.normpath(
-                                        os.path.join(self.www_path +
-                                                     first_line_data[1]))
+                requested_path = os.path.join(requested_path, "index.html")
 
-            if (os.path.isfile(requested_file_path) and
-                    requested_file_path.startswith(self.www_path)):
+            # If requested path is a file and is contained in ./www, read from
+            # it and set as response's message body.
+            if (os.path.isfile(requested_path) and
+                    requested_path.startswith(self.www_path)):
+
+                 # Set the mimetypes depending on the request
+                if requested_path.endswith(".html"):
+                    self.content_type += "text/html\n"
+                elif requested_path.endswith(".css"):
+                    self.content_type += "text/css\n"
+                else:
+                    self.content_type += "text/plain\n"
+
                 self.status_line += "200 OK\n"
-                file = open(requested_file_path)
 
+                file = open(requested_path)
                 self.message_body = file.read()
-            else:
-                self.status_line += "404 NOT FOUND\n"
+                file.close()
+
+            # If requested path is a directory and is contained in ./www,
+            # send a redirect response with a trailing slash appended to the
+            # original request URL.
+            elif (os.path.isdir(requested_path) and
+                    requested_path.startswith(self.www_path)):
+                self.status_line += "301 Moved Permanently\n"
                 self.content_type += "text/html\n"
-                self.message_body = "<html><body>HTTP/1.1 404 NOT FOUND" \
+                self.location = "Location: " + first_line_data[1] + "/\n"
+                self.message_body = "<html><body><h1>310 MOVED PERMANENTLY" \
+                                    "</h1><a href=\"" + first_line_data[1] + \
+                                    "/" + "\">Click to get redirected</a>" \
                                     "</body></html>\n"
+
+            # Otherwise, construct a 404 not found response.
+            else:
+                self.status_line += "404 Not Found\n"
+                self.content_type += "text/html\n"
+                self.message_body = "<html><body><h1>404 NOT FOUND" \
+                                    "</h1></body></html>\n"
 
         # Calculate the content length of the message body
         self.content_length = self.content_length + \
-                              str(len(self.message_body)) + "\n"
+                              str(len(self.message_body)) + "\n\n"
 
+        # Send out the response
         self.request.sendall(self.status_line + self.get_datetime() +
-                             self.content_type + self.content_length + "\n" + \
-                             self.message_body)
-
-    def get_datetime(self):
-        return "Date: " + strftime("%a, %d %b %Y %X GMT", gmtime()) + "\n"
+                             self.location + self.content_type +
+                             self.content_length + self.message_body)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
